e78fda4f9757b4ce7dfbf0b015655b7c45e30418
canadmin <icanyard@gmail.com>
2019-05-13 21:07:43 +0300

    hello world

---
+++ Jsf/src/com/jsf/PersonBean.java
@@ -0,0 +1,22 @@
+package com.jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class PersonBean {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
