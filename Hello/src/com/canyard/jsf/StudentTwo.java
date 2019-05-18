f595b91ad0e66a07af38ccad99e15ea8a634723e
canadmin <icanyard@gmail.com>
2019-05-18 23:49:50 +0300

    radio button jsf-uı

--- Hello/src/com/canyard/jsf/StudentTwo.java
+++ Hello/src/com/canyard/jsf/StudentTwo.java
@@ -10,23 +10,36 @@ public class StudentTwo {
 	private String firstName;
 	private String lastName;
 	private String country;
+	private String favoriteLanguage;
 	
 	List<String> countryOptions;
+	List<String> radioOptions;
 	
 	public StudentTwo() {
 		countryOptions=new ArrayList<>();
-		
 		countryOptions.add("Turkiye");
 		countryOptions.add("Fransa");
 		countryOptions.add("Amerika");
+		
+		radioOptions=new ArrayList<>();
+		
+		radioOptions.add("Java");
+		radioOptions.add("C++");
 	}
 	
+	
+
+	public List<String> getRadioOptions() {
+		return radioOptions;
+	}
+
 
 
 	public List<String> getCountryOptions() {
 		return countryOptions;
 	}
 
+	
 
 
 	public String getFirstName() {
@@ -57,6 +70,19 @@ public class StudentTwo {
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
+
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+	
 	
 	
 	
